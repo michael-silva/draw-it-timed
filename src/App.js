63ac1ffe0cac9957aa1c6a4af77500491c55048e
Michael Silva <michaelpereiradasilva@hotmail.com>
2021-12-16 09:21:00 -0300

    feat: create a terms page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
+import { Terms } from './pages/Terms'
+import { Main } from './pages/Main'
+
+const isDevelop = process.env.NODE_ENV === 'development'
 
 function App() {
   return (
+    <BrowserRouter basename={isDevelop ? '/' : process.env.PUBLIC_URL}>
     <div className="App">
       <header className="App-header">
+        <div className="App-title">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Draw it Timed!</h1>
+        </div>
+        <nav className="App-navbar">
+          <ul>
+            <li>
+              <Link to="/" className="App-link">Main</Link>
+            </li>
+            <li>
+              <Link to="/terms" className="App-link">Terms</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+      <Routes>
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </main>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,5 @@
+const Main = () => {
+    return <h2>Main page</h2>
+}
+
+export default Main
